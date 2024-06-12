9ea02b639ba3e656f2459d3a6b1f85553e522586
unknown <coolshreyans01@gmail.com>
2024-06-13 00:37:02 +0530

    working on show chats v1.1

--- controllers/chat.js
+++ controllers/chat.js
@@ -15,7 +15,6 @@ module.exports.getChats = async (req, res) => {
   // }
   try {
     const allChats = await Message.find().sort({ timestamp: 1 });
-    res.json(allChats);
     res.render("./chats/showMessage.ejs", { allChats });
   } catch (error) {
     console.error("Error retrieving chats:", error);
