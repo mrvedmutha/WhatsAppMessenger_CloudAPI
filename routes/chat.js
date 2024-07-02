793df6909f582d9e619be0ccc5d05e6c1ea5d264
Shreyans Jain <coolshreyans01@gmail.com>
2024-07-02 16:33:27 +0530

    working on show chats v1.2.10

--- routes/chat.js
+++ routes/chat.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const chatController = require("../controllers/chat.js");
 
+app.get("/", chatController.getChats);
 router.get("/chats", chatController.getChats);
 router.post("/send", chatController.sendMessage);
 router.post("/webhook", chatController.webhook);
